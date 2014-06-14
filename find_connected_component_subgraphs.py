9055a9c926238f13f59fc3e4b4db29250e64cf18
satarupaguha11 <satarupaguha11@gmail.com>
2014-06-14 20:28:49 +0530

    add functionality to handle gexf file format.

--- find_connected_component_subgraphs.py
+++ find_connected_component_subgraphs.py
@@ -14,3 +14,10 @@ def connected_subgraph_pajek(input_graph_name):
 	connected_graph=nx.connected_component_subgraphs(graph)[0]
 	return connected_graph
 
+def connected_subgraph_gexf(input_graph_name):
+
+	graph=nx.read_gexf(input_graph_name)
+	connected_graph=nx.connected_component_subgraphs(graph)[0]
+	return connected_graph
+
+
